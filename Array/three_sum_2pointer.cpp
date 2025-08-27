4891fba4d6a47d46a233c1a4f8b5eb171c1830b2
Kapil <kydv901@gmail.com>
2025-08-28 00:08:22 +0530

    new commit

---
+++ Array/three_sum_2pointer.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void threeSum(vector<int>& v, int target) {
+    // MUST sort the array first
+    sort(v.begin(), v.end());
+    
+    for(int i = 0; i < v.size() - 2; i++) {  //here v.size() - 2 , because after that there will be two elements left , which can form triplet.
+        int left = i + 1;
+        int right = v.size() - 1;
+        
+        while(left < right) {
+            int sum = v[i] + v[left] + v[right];
+            
+            if(sum == target) {
+                cout << "Found: " << v[i] << ", " << v[left] << ", " << v[right] << endl;
+                left++;
+                right--;
+            }
+            else if(sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+    }
+}
+
+int main(){
+      int n;
+    cout<<"enter n : ";cin>>n;
+    vector<int> v(n);
+    for(int i=0 ; i<n ; i++){
+        cout<<"Enter the "<<i<<" element : ";cin>>v[i];
+    }
+    int target = 200;
+    threeSum(v,target);
+    return 0;
+}
--- Array/two_sum_using2pointer.cpp
+++ Array/two_sum_using2pointer.cpp
@@ -10,6 +10,7 @@ void my_function(vector<int> &v , int target){
     int sum = 0;
     int left = 0; //points to starting.
     int right = v.size() - 1;  //points to the end of array.
+    bool found = false;
 
     while(left < right){  //As long as the two pointers don’t cross each other, keep checking the sum of the current two numbers.
         sum = v[left] + v[right];
@@ -17,6 +18,12 @@ void my_function(vector<int> &v , int target){
         if(sum == target){
             cout<<"Found : "<<v[left]<<" and "<<v[right]<<endl;
             left++,right --; //move both pointers inward to keep searching.
+            found = true;
+
+            while (left < right && v[left] == v[left + 1]) left++; //skip duplicate values for the left pointer.
+            
+
+            while (left < right && v[right] == v[right - 1]) right--; //skip duplicates for the right pointer.
         }
         else if (sum < target){
             left++; //if sum is too small move the pointer to the right 
@@ -24,6 +31,9 @@ void my_function(vector<int> &v , int target){
         else{
             right--; //if sum is too large  , move the pointer to the left.
         }
+    }
+    if(!found){
+        cout<<"No Pairs found."<<endl;
     }
 }
 int main(){
