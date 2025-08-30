8993ebcd515abcfb395ea1d025b062111ad6f536
Kapil <kydv901@gmail.com>
2025-08-30 14:32:31 +0530

    updated to handle negative values

--- Array/longest_subarr_k_sum.cpp
+++ Array/longest_subarr_k_sum.cpp
@@ -21,6 +21,11 @@ int main(){
         for(int j = i; j < size; j++){
             //Together, i and j define one unique subarray. For example, when i=1 and j=3, you are looking at the subarray {2, 3, 4}.
             int sum = 0;
+
+            // arr = {1,2,3,4};
+            // the possible subarr = {1},{1,2},{1,2,3},{1,2,3,4} / {2},{2,3},{2,3,4} / {3},{3,4} / {4};
+            // i will be pointing to the single element like 1,2,3 and j will be travesring to all elements left to make subarray like - 1,2 / 1,2,3 / 1,2,3,4.
+
             
             for(int k = i; k <= j; k++){ //to add up the numbers in the current subarray defined by i and j
                 sum += v[k]; //takes the number at the current position k in the list and adds it to the sum.
