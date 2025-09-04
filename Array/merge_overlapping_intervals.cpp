6a6ce34e0de6ea89772126e7c441e06df4f906a5
Kapil <kydv901@gmail.com>
2025-09-04 22:52:42 +0530

    updated

--- Array/merge_overlapping_intervals.cpp
+++ Array/merge_overlapping_intervals.cpp
@@ -10,7 +10,7 @@ vector<vector<int>> mergeOverlapping(vector<vector<int>>& arr) {
     vector<vector<int>> ans;
     
     for(int i = 0; i < n; i++) {
-        // If ans is empty OR no overlap with previous interval
+        // If ans is empty OR no overlap with previous interval.
         //arr[i][0] : for the ith interval , the first element of that.
         //ans.back() : the previous interval.
         //ans.back()[1] : second element of the previous interval.
@@ -19,7 +19,7 @@ vector<vector<int>> mergeOverlapping(vector<vector<int>>& arr) {
             ans.push_back(arr[i]);
         }
         // If overlap, merge with previous interval
-        else {
+        else if(arr[i][0] <= ans.back()[1]){
             ans.back()[1] = max(ans.back()[1], arr[i][1]);
         }
     }
@@ -32,7 +32,7 @@ int main() {
     vector<vector<int>> v = {{1,3}, {2,4}, {5,6}};
     
     cout << "Input: ";
-    for(auto interval : v) {
+    for(auto interval : v){
         cout << "[" << interval[0] << "," << interval[1] << "] ";
     }
     cout << endl;
@@ -40,8 +40,8 @@ int main() {
     auto result = mergeOverlapping(v);
     
     cout << "Output: ";
-    for(auto interval : result) {
-        cout << "[" << interval[0] << "," << interval[1] << "] ";
+    for(auto i : result) {
+        cout << "[" << i[0] << "," << i[1] << "] ";
     }
     cout << endl;
     
