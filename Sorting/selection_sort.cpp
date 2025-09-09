e0e9b7481e9bd40598acc0ec99ee3481064a01c7
Kapil <kydv901@gmail.com>
2025-09-10 00:19:33 +0530

    selection sort updated

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -5,14 +5,17 @@ int main(){
     vector<int> v = {1,35,76,21,39,10,99};
     int n = v.size();
     //implementing the selection sort.
-    for(int i=0 ; i<n ; i++){
-        int small = i;
-        for(int j=i+1 ; j<n ; j++){
-            if(v[j] < v[small]){
-                small = j;
+    //it divides the array into the two parts - one is smaller element and other one is unsorted array.
+    for(int i=0 ; i<n ; i++){ //to change the position of the small from starting to the ending of the array.
+        int small = i; //assuming that the current position has the smallest value.
+
+
+        for(int j=i+1 ; j<n ; j++){ //inner loop , check the remaining elements.
+            if(v[j] < v[small]){ //if we find the smaller element.
+                small = j; //update the position of the smallest element.
             }
         }
-        swap(v[i],v[small]);
+        swap(v[i],v[small]); //swap the element with the current position.
     }
     
     cout<<"The Vector After Applying the Selection Sort : "<<endl;
