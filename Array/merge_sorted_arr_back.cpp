77dcaa8a2b41a657010c4ca5666c7ea7e1ba34c5
Kapil <kydv901@gmail.com>
2025-09-17 13:36:10 +0530

    merging frmo backsidE

---
+++ Array/merge_sorted_arr_back.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;       // pointer for nums1
+        int j = n - 1;       // pointer for nums2
+        int k = m + n - 1;   // pointer for placement in nums1
+
+        // Merge from the back
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // If nums2 still has elements, copy them
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+};
