fb9a2ee769f372175e17372569736092c553468b
Kapil <kydv901@gmail.com>
2025-09-20 00:18:56 +0530

    lower bound updated

--- Searching/lower_bound_impl.cpp
+++ Searching/lower_bound_impl.cpp
@@ -9,10 +9,10 @@ int searchx(vector<int>&v,int k){
 
         if(v[mid] >= k){
             ans = mid;
-            left = mid+1;
+            right = mid-1;
         }
         else{
-            right = mid-1;
+            left = mid+1;
         }
     }
     return ans;
