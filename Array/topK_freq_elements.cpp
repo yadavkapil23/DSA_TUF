603f3e614a220bf454e5d25d2fb5bc47946356ff
Kapil <kydv901@gmail.com>
2025-10-19 10:49:26 +0530

    fixed the code by adding comparator

--- Array/topK_freq_elements.cpp
+++ Array/topK_freq_elements.cpp
@@ -1,29 +1,37 @@
-class Solution {
-public:
-    vector<int> topKFrequent(vector<int>& v, int k) {
-        int n = v.size();
-        map<int,int> m;
-        vector<int> result;
-
-        for(int i=0 ; i<n ; i++){ //using map to calculate count.
-            m[v[i]]++;
-        }
-
-        vector<pair<int,int>> freq; //converting count to pairs -> count , element
-        for (auto &p : m) {
-            freq.push_back({p.second, p.first});
-        }
-
-        //sorting by freq.
-        sort(freq.begin(), freq.end(), greater<pair<int,int>>()); //to sort in descending order.
-        //we couldnt use the f.end() to f.begin() ,bcz begin always comes before the end , so thats why we use greater 
-
-
-        //top K
-        for(int i = 0; i < k; i++) {
-            result.push_back(freq[i].second);
-        }
-        
-        return result;
+//to find the top K freq elements
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+bool compare(const pair<int, int>& a, const pair<int, int>& b) {
+    return a.second > b.second;
+}
+vector<int> KFreq(vector<int> &v,int k){
+    vector<int> result;
+    map<int,int> m;
+    for(auto i:v){ //to count.
+        m[i]++;
     }
-};
+    
+    //creating a vector to store those key value pairs of number and its count.
+    vector<pair<int,int>> freq;
+    for(auto &p : m){
+        freq.push_back({p.first,p.second});
+    }
+    
+    sort(freq.begin(),freq.end(),compare);
+    
+    for(int i=0 ; i<k ; i++){
+        result.push_back(freq[i].first);
+    }
+    return result;
+}
+int main(){
+    vector<int> v = {1, 32, 5, 23, 32, 1, 1, 2, 2, 11};
+    
+    cout << "Top 2 frequent elements are: ";
+    for(int num : KFreq(v,2)) {
+        cout << num << " ";
+    }
+}
