90c2f1469b77c1a15f6e99a930436a2f61561f3b
Kapil <kydv901@gmail.com>
2025-10-19 12:41:30 +0530

    Implemented Upper Bound

---
+++ Searching/upper_bound.cpp
@@ -0,0 +1,19 @@
+int searchx(vector<int>&v,int k){
+    int n = v.size();
+    int inserted = 9;
+    int left = 0;
+    int right = n-1;
+    int ans = n;
+    while(left <= right){
+        int mid = left + (right-left)/2;
+
+        if(v[mid] <= k){
+            ans = mid;
+            left = mid+1;
+        }
+        else{
+            right = mid-1;
+        }
+    }
+    return ans;
+}
