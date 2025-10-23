9f6b7c8eff7baba60bb72ec8400d596ddcd20089
Kapil <kydv901@gmail.com>
2025-10-23 23:50:22 +0530

    palindrome

--- Recursion/check_palindrome.cpp
+++ Recursion/check_palindrome.cpp
@@ -1,34 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-// Recursive function to check if a string is a palindrome
 bool isPalindrome(string str, int start, int end) {
-    // Base case
     if (start >= end) {
         return true;
     }
-
-    // If characters don't match
     if (str[start] != str[end]) {
         return false;
     }
-
-    // Recursive case
     return isPalindrome(str, start + 1, end - 1);
 }
 
 int main() {
     string input;
-    
     cout << "Enter a string to check if it's a palindrome: ";
-    getline(cin, input); // allows spaces
-    
+    getline(cin, input);
     if (isPalindrome(input, 0, input.length() - 1)) {
         cout << "\"" << input << "\" is a palindrome!" << endl;
     } else {
         cout << "\"" << input << "\" is not a palindrome." << endl;
     }
-
     return 0;
 }
