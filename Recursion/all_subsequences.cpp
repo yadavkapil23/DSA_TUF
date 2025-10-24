aa2a2adb0fbbadb841dd939101696caaf5db18f1
Kapil <kydv901@gmail.com>
2025-10-24 22:53:11 +0530

    subsequences

---
+++ Recursion/all_subsequences.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+void printSubsequences(string input, string output, int index) {
+    if (index == input.size()) {
+        cout << output << endl;  // print the current subsequence
+        return;
+    }
+    //Include the current character
+    printSubsequences(input, output + input[index], index + 1);
+
+    //Exclude the current character
+    printSubsequences(input, output, index + 1);
+}
+int main() {
+    string s = "abc";
+    cout << "All Subsequences are:\n";
+    printSubsequences(s, "", 0);
+    return 0;
+}
