868cac30b1bccbfd89d50ee8b02203bee110b597
Kapil <kydv901@gmail.com>
2025-10-24 11:05:30 +0530

    Fibonacci Series

---
+++ Recursion/fibonacci.cpp
@@ -0,0 +1,25 @@
+//fibonacci using recursion.
+#include<iostream>
+using namespace std;
+int fibonacci(int n){
+    if(n<0){
+        cout<<"Input sHOULD Be positive";
+    }
+    else if(n==0){
+        return 0;
+    }
+    else if(n==1){
+        return 1;
+    }
+    else{
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+}
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0 ; i<n ; i++){
+    cout<<fibonacci(i)<<" ";
+    }
+    
+}
