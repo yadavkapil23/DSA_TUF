e57bcf60815ac7777b1b3b9ca7ca73e159aa0c15
Kapil <kydv901@gmail.com>
2025-10-27 22:26:35 +0530

    Subseq of vector

---
+++ Recursion/all_subseq_vector.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void printsequences(string input , string output,int index){
+    if(index == input.size()){
+        cout<<output<<endl;
+        return;
+    }
+    
+    output.push_back(input[index]);
+    printsequences(input,output + input[index],index+1); //considering the sequence.
+
+    output.pop_back();
+    printsequences(input,output,index+1);
+}
+int main(){
+    string s = "Kapil";
+    printsequences(s,"",0);
+    return 0;
+}
