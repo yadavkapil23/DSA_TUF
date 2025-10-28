d832a62376767c513f88dc890a5e0dd91aefac3c
Kapil <kydv901@gmail.com>
2025-10-29 00:41:04 +0530

    updated

--- Array/product_of_arr_except_self.cpp
+++ Array/product_of_arr_except_self.cpp
@@ -4,33 +4,28 @@ using namespace std;
 
 vector<int> productExceptSelf(vector<int>& nums) {
     int n = nums.size();
-    vector<int> output(n, 1);
+    vector<int> ans(n, 1);
 
-    // Step 1: Calculate left products
-    // output[i] will temporarily store the product of all numbers to the left of i
-    for (int i = 1; i < n; i++) {
-        output[i] = output[i - 1] * nums[i - 1];
+    int left = 1;
+    for (int i = 0; i < n; i++) {
+        ans[i] = left;        // store product of all elements to the left of i
+        left *= nums[i];      // update left product for next index
     }
 
-    // Step 2: Multiply by right products using a running variable
-    int rightProduct = 1; // no numbers to the right of last element
+    int right = 1;
     for (int i = n - 1; i >= 0; i--) {
-        output[i] *= rightProduct;  // multiply left product with right product
-        rightProduct *= nums[i];    // update right product for next iteration
+        ans[i] *= right;      // multiply by product of all elements to the right of i
+        right *= nums[i];     // update right product for previous index
     }
 
-    return output;
+    return ans;
 }
 
 int main() {
     vector<int> nums = {1, 2, 3, 4};
     vector<int> result = productExceptSelf(nums);
 
-    cout << "Output: ";
-    for (int val : result) {
+    for (int val : result)
         cout << val << " ";
-    }
-    cout << endl;
-
     return 0;
 }
