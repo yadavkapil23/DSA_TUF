fa3d16ae63dfc5a5bc1b4d5e19de6c3c962d246f
Kapil <kydv901@gmail.com>
2025-10-29 09:33:51 +0530

    corrected code

--- Recursion/combination_sum.cpp
+++ Recursion/combination_sum.cpp
@@ -1,28 +1,47 @@
-class Solution {
-public:
-    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds) {
-        if (ind == arr.size()) {
-            if (target == 0) {
-                ans.push_back(ds);
-            }
-            return;
-        }
+#include <iostream>
+#include <vector>
+using namespace std;
 
-        // pick up the element
-        if (arr[ind] <= target) {
-            ds.push_back(arr[ind]);
-            findCombination(ind, target - arr[ind], arr, ans, ds);
-            ds.pop_back();
+void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds) {
+    if (ind == arr.size()) {
+        if (target == 0) {
+            ans.push_back(ds);
         }
-
-        findCombination(ind + 1, target, arr, ans, ds);
+        return;
     }
 
-public:
-    vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        findCombination(0, target, candidates, ans, ds);
-        return ans;
+    // pick the element
+    if (arr[ind] <= target) {
+        ds.push_back(arr[ind]);
+        findCombination(ind, target - arr[ind], arr, ans, ds);
+        ds.pop_back();
     }
-};
+
+    // move to next index
+    findCombination(ind + 1, target, arr, ans, ds);
+}
+
+vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
+    vector<vector<int>> ans;
+    vector<int> ds;
+    findCombination(0, target, candidates, ans, ds);
+    return ans;
+}
+
+int main() {
+    vector<int> candidates = {2, 3, 6, 7};
+    int target = 7;
+
+    vector<vector<int>> result = combinationSum(candidates, target);
+
+    cout << "Combinations that sum to " << target << ":\n";
+    for (auto &combination : result) {
+        cout << "[ ";
+        for (int num : combination) {
+            cout << num << " ";
+        }
+        cout << "]\n";
+    }
+
+    return 0;
+}
