0d3351836504985e99a77f724454c2bff3b6f498
Kapil <kydv901@gmail.com>
2025-10-29 10:22:23 +0530

    new commit

--- Recursion/combination_sum.cpp
+++ Recursion/combination_sum.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 using namespace std;
 
-void findCombination(int ind, int target, vector<int> &candidates, vector<vector<int>> &ans, vector<int> &ds) {
-    if (ind == candidates.size()) {
+void findCombination(int ind, int target, vector<int> &v, vector<vector<int>> &ans, vector<int> &ds) {
+    if (ind == v.size()) {
         if (target == 0) {
             ans.push_back(ds);
         }
@@ -11,29 +11,29 @@ void findCombination(int ind, int target, vector<int> &candidates, vector<vector
     }
 
     // pick the element
-    if (candidates[ind] <= target) {
-        ds.push_back(candidates[ind]);
-        findCombination(ind, target - candidates[ind], candidates, ans, ds); //recursion to find all possible combination starting with candidates[ind].
+    if (v[ind] <= target) {
+        ds.push_back(v[ind]);
+        findCombination(ind, target - v[ind], v, ans, ds); //recursion to find all possible combination starting with v[ind].
         ds.pop_back(); //pop_back() removes the last added element to undo the previous choice, so we can try other possibilities correctly.
         //like suppose if the sum is not equal to target and the index > required sum to match target, then we remove its previous element from ds and try another possiblity.
     }
  
     // move to next index
-    findCombination(ind + 1, target, candidates, ans, ds);
+    findCombination(ind + 1, target, v, ans, ds);
 }
 
-vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
+vector<vector<int>> combinationSum(vector<int> &v, int target) {
     vector<vector<int>> ans;
     vector<int> ds;
-    findCombination(0, target, candidates, ans, ds);
+    findCombination(0, target, v, ans, ds);
     return ans;
 }
 
 int main() {
-    vector<int> candidates = {2, 3, 6, 7};
+    vector<int> v = {2, 3, 6, 7};
     int target = 7;
 
-    vector<vector<int>> result = combinationSum(candidates, target);
+    vector<vector<int>> result = combinationSum(v, target);
 
     cout << "Combinations that sum to " << target << ":\n";
     for (auto &combination : result) {
