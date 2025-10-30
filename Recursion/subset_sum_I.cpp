8c90222338e424a5757976ef1d8892110ad188d3
Kapil <kydv901@gmail.com>
2025-10-30 09:23:57 +0530

    updated

--- Recursion/subset_sum_I.cpp
+++ Recursion/subset_sum_I.cpp
@@ -3,31 +3,31 @@
 #include<algorithm>
 using namespace std;
 
-void func(int ind, int sum, vector<int> &arr, int N, vector<int> &sumSubset) {
+void func(int ind, int sum, vector<int> &v, int N, vector<int> &ans) {
     if (ind == N) {
-        sumSubset.push_back(sum);
+        ans.push_back(sum);
         return;
     }
 
     // pick the element
-    func(ind + 1, sum + arr[ind], arr, N, sumSubset);
+    func(ind + 1, sum + v[ind], v, N, ans);
 
     // do not pick the element
-    func(ind + 1, sum, arr, N, sumSubset);
+    func(ind + 1, sum, v, N, ans);
 }
 
-vector<int> subsetSums(vector<int> &arr, int N) {
-    vector<int> sumSubset;
-    func(0, 0, arr, N, sumSubset);
-    sort(sumSubset.begin(), sumSubset.end());
-    return sumSubset;
+vector<int> subsetSums(vector<int> &v, int N) {
+    vector<int> ans;
+    func(0, 0, v, N, ans);
+    sort(ans.begin(), ans.end());
+    return ans;
 }
 
 int main() {
-    vector<int> arr = {3, 1, 2};
-    int N = arr.size();
+    vector<int> v = {3, 1, 2};
+    int N = v.size();
     
-    vector<int> ans = subsetSums(arr, N);
+    vector<int> ans = subsetSums(v, N);
     
     cout << "Subset sums: ";
     for (auto it : ans) cout << it << " ";
