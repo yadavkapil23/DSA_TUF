78e4b8b3bb7e1e65352526b64de02be66bedcec7
Kapil <kydv901@gmail.com>
2025-11-08 21:33:34 +0530

    new code

---
+++ Linked_List/DLL_delete_from_beg.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node* prev;
+
+    node(int val){
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+//inserting data;
+void insert(node* &head,int val) {
+	node* newnode = new node(val);
+	if(head==nullptr) {
+		head=newnode;
+		return;
+	}
+	node* temp = head;
+	while(temp->next != nullptr) {
+		temp = temp->next;
+	}
+	temp->next=newnode;
+	newnode->prev=temp;
+}
+
+//printing the data.
+void printing(node* &head) {
+	if(head==nullptr) {
+		return;
+	}
+    node* temp = head;
+	while(temp != nullptr) {
+		cout<<temp->data<<" ";
+		temp=temp->next;
+	}
+}
+int main() {
+	node* head = nullptr;
+	int val;
+	char ch;
+	cout<<"Do you want to insert node : ";
+	cin>>ch;
+	while(ch=='y'||ch=='Y') {
+		cout<<"Enter data : ";
+		cin>>val;
+		insert(head,val);
+		cout<<"Do you want to enter more data : ";
+		cin>>ch;
+	}
+	
+	int x;
+	cout<<"Do you want to insert more data : ";cin>>ch;
+	cout<<"Enter the data to insert : ";cin>>x;
+	printing(head);
+	
+	
+}
