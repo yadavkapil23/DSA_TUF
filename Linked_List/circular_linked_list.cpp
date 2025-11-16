8d4513ef1943a4a8d355e6792a3e826f3219eb00
Kapil <kydv901@gmail.com>
2025-11-17 02:40:20 +0530

    updated

--- Linked_List/circular_linked_list.cpp
+++ Linked_List/circular_linked_list.cpp
@@ -13,6 +13,7 @@ void inserting(node* &head,int val){
     node* newnode = new node(val);
     if(head==nullptr){
         head=newnode;
+        newnode->next=head;
     }
     node* temp = head;
     while(temp->next != head){
@@ -21,6 +22,28 @@ void inserting(node* &head,int val){
     temp->next=newnode;
     newnode->next=head;
 }
-int main(){
 
+void printing(node* &head){
+node* temp = head;
+if(head==nullptr){
+    return;
+}
+do{
+    cout<<temp->data<<" ";
+    temp = temp->next;
+}
+while(temp != head);
+}
+int main(){
+node* head = nullptr;
+int val;
+char ch;
+cout<<"do you want to create CLL : ";cin>>ch;
+while(ch=='y'||ch=='Y'){
+    cout<<"enter the data : ";cin>>val;
+    inserting(head,val);
+    cout<<"do you want to enter more nodes : ";cin>>ch;
+}
+printing(head);
+return 0;
 }
