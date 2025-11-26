dc60a0ed2c0ef9433eda8e9efbd0c2d6763c334b
yadavkapil23 <kydv901@gmail.com>
2025-11-26 10:06:33 +0530

    updated

--- Stack/stack_using_LL.cpp
+++ Stack/stack_using_LL.cpp
@@ -1,26 +1,27 @@
+//now trying for the stack using the linked list.
 #include<iostream>
 using namespace std;
-struct node {
+struct node{
     int data;
     node* next;
 };
 
-struct Stack {
-    node* top = nullptr;
+struct stack{
     int size = 0;
+    node* top = nullptr;
 };
 
-void push(Stack &s, int x) {
+void pushing(stack &s,int x){
     node* temp = new node;
-    temp->data = x;
-    temp->next = s.top;
-    s.top = temp;
+    temp->data=x;
+    temp->next=s.top;
+    s.top=temp;
     s.size++;
 }
 
-void pop(Stack &s) {
-    if (s.top == nullptr) {
-        cout << "Stack Underflow\n";
+void pop(stack &s){
+    if(s.top == nullptr){
+        cout<<"empty stack.";
         return;
     }
     node* temp = s.top;
@@ -29,16 +30,52 @@ void pop(Stack &s) {
     s.size--;
 }
 
-int peek(Stack &s) {
-    if (s.top == nullptr) {
-        cout << "Stack empty\n";
+int getTop(stack &s){
+    if(s.top == nullptr){
+        cout<<"Empty";
         return -1;
     }
-    return s.top->data;
+    else{
+        return s.top->data;
+    }
 }
 
-int getSize(Stack &s) {
-    return s.size;
+int getsize(stack &s){
+    if(s.top == nullptr){
+        cout<<"Empty";
+        return -1;
+    }
+    else{
+        return s.size;
+    }
 }
 
-int main(){}
+void display(stack &s){
+    node* temp = s.top;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main(){
+    stack s;
+
+    pushing(s, 10);
+    pushing(s, 20);
+    pushing(s, 30);
+
+    cout << "Top: " << getTop(s) << endl;
+    cout << "Size: " << getsize(s) << endl;
+
+    display(s);
+
+    pop(s);
+
+    cout << "After pop, Top: " << getTop(s) << endl;
+    cout << "Size: " << getsize(s) << endl;
+
+    display(s);
+
+    return 0;
+}
