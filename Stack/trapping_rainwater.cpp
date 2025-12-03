cbcb9dadb966e0f35d3411880ea21760b8cd6c85
yadavkapil23 <kydv901@gmail.com>
2025-12-03 11:13:59 +0530

    Trapping Rainwater Using Stack

--- Stack/trapping_rainwater.cpp
+++ Stack/trapping_rainwater.cpp
@@ -10,11 +10,9 @@ public:
         int current = 0;
         
         while (current < height.size()) {
-            while (!st.empty() && height[current] > height[st.top()]) {
-                // 1. Get Bottom
+            while (!st.empty() && height[current] > height[st.top()]) { //keep popping until we find an element that is not bigger or equal to right element.
                 int bottom = st.top();
                 st.pop();
-
                 // 2. Check Left Wall
                 if (st.empty()) {
                     break;
@@ -24,7 +22,7 @@ public:
                 int left = st.top();
                 
                 // 4. Calculate
-                int width = current - left - 1;
+                int width = current - left - 1; // -1 is here , just to exclude the walls while counting the distance and only include the distance or units between them.
                 int bounded_height = min(height[left], height[current]) - height[bottom];
                 
                 total_water += width * bounded_height;
