5ab1e43df599bd337bdeb3e821f310a6fc79f263
yadavkapil23 <kydv901@gmail.com>
2025-12-06 22:09:43 +0530

    Stock Span

---
+++ Stack/stock_span.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+vector<int> solution(vector<int> &v){
+    stack<int> st;
+    vector<int> span;
+    int n = v.size();
+    for(int i=0 ; i<n ; i++){
+        while(!st.empty() && v[st.top()] <= v[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            span.push_back(i+1);
+        }
+        else{
+            span.push_back(i-st.top());
+        }
+        st.push(i);
+    }
+    return span;
+}
+int main(){
+    vector<int> v={100,80,60,70};
+    vector<int> ans = solution(v);
+    
+    for(auto i : ans){
+        cout<<i<<endl;
+    }
+}
