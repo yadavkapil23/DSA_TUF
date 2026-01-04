664bcb6569947c653481331ebbe93cde8f421412
yadavkapil23 <kydv901@gmail.com>
2026-01-04 14:23:21 +0530

    Longest Common Prefix

--- Array/longest_common_prefix.cpp
+++ Array/longest_common_prefix.cpp
@@ -1,14 +1,43 @@
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
-        string prefix = strs[0];
-        for (int i = 1; i < strs.size(); i++) {
-            while (strs[i].find(prefix) != 0) {
-                prefix = prefix.substr(0, prefix.size() - 1);
-                if (prefix.empty()) return "";
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+
+    // If there are NO words, answer is empty
+    if (strs.size() == 0)
+        return "";
+
+    // We take the FIRST word as reference
+    // strs[0] means the first word
+    // Example: strs[0] = "flower"
+    
+    // i = which LETTER we are checking (0, 1, 2, ...)
+    for (int i = 0; i < strs[0].size(); i++) {
+
+        // Take the letter at position i from the first word
+        // Example: i = 0 → 'f'
+        char ch = strs[0][i];
+
+        // j = which WORD we are checking (1st, 2nd, 3rd...)
+        // We start from j = 1 because j = 0 is the first word itself
+        for (int j = 1; j < strs.size(); j++) {
+
+            // Two reasons to STOP:
+            // 1) This word is too short to have a letter at position i
+            // 2) This word has a DIFFERENT letter at position i
+            if (i >= strs[j].size() || strs[j][i] != ch) {
+
+                // substr(0, i) means:
+                // take the first i letters of the first word
+                // Example: "flower".substr(0, 2) → "fl"
+                return strs[0].substr(0, i);
             }
         }
-        return prefix;
     }
-};
+
+    // If we never stopped, the whole first word is common
+    return strs[0];
+}
