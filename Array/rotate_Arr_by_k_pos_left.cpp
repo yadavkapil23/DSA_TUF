157850580bd00c63afd1a85c604608c937f7c00e
Kapil <kydv901@gmail.com>
2026-02-17 20:56:26 +0530

    reverse by k pos left

---
+++ Array/rotate_Arr_by_k_pos_left.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+void reversing_byk(vector<int> &v,int k) {
+	int n = v.size();
+
+	k = k%n;
+
+	reverse(v.begin(),v.begin()+k);
+	reverse(v.begin()+k,v.end());
+    reverse(v.begin(),v.end());
+}
+
+int main() {
+	int n;
+	cin>>n;
+	int k;
+	cin>>k;
+
+	vector<int> v;
+
+	int a;
+	for(int i=0 ; i<n ; i++) {
+		cin>>a;
+		v.push_back(a);
+	}
+
+	reversing_byk(v,k);
+
+	for(int i=0 ; i<n ; i++) {
+		cout<<v[i]<<" ";
+	}
+	return 0;
+}
