df2e48861f19c57c0fc1cd38ff2c7963006d0c0d
Kapil <kydv901@gmail.com>
2026-02-23 13:48:01 +0530

    generate substr

--- Basic_Questions/generate_all_substrings.cpp
+++ Basic_Questions/generate_all_substrings.cpp
@@ -13,5 +13,11 @@ int main(){
         }
     }
     
+
+    // //another way
+    //     for(int i = 0; i < n; i++){
+    //     for(int j = i; j < n; j++) {
+    //         cout<<s.substr(i,j);
+    //     }
     return 0;
 }
