dce828e3dcc4e5472cb85577f64f13e6ac2def36
Kapil <kydv901@gmail.com>
2026-03-29 14:45:10 +0530

    Find depth of a node

---
+++ Tree/depth_of_a_node.cpp
@@ -0,0 +1,36 @@
+#include<algorithm>
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node* left;
+    node* right;
+
+    node(int val){
+        data = val;
+        left=right=nullptr;
+    }
+};
+
+int depth(node* root,int target,int level=0){
+    if(root==nullptr){
+        return 0;
+    }
+    
+    if(root->data==target){
+        return level;
+    }
+
+    int left = depth(root,target,level+1);
+    if(left != -1){
+        return left;
+    }
+
+    int right = depth(root,target,level+1);
+    return right;
+}
+
+
+//UNLIKE MAXDEPTH , here we are finding the level at which the target node is present.
+//You are NOT finding longest path
+// You are finding distance to ONE specific node
