62f7548d9e2bab20018ffcc070a6d64a350ab689
Kapil <kydv901@gmail.com>
2026-04-05 22:34:15 +0530

    Refactor boundary traversal functions in tree

--- Tree/boundary_traversal_tree.cpp
+++ Tree/boundary_traversal_tree.cpp
@@ -1,4 +1,6 @@
-#include <vector>
+#include<vector>
+#include<iostream>
+#include<algorithm>
 using namespace std;
 
 // Assuming Node structure is defined like this:
@@ -8,71 +10,60 @@ struct Node {
     Node* right;
     Node(int x) : data(x), left(nullptr), right(nullptr) {}
 };
-
-// Helper: Check if node is a leaf
-bool isLeaf(Node* node) {
-    return !node->left && !node->right;
+bool isLeaf(Node* root){
+    return !root->left && !root->right;
 }
-
-// Add left boundary nodes (excluding leaves)
-void addLeftBoundary(Node* root, vector<int>& res) {
-    Node* cur = root->left;
-    while (cur) {
-        if (!isLeaf(cur)) 
-            res.push_back(cur->data);
-        if (cur->left)
-            cur = cur->left;
-        else
-            cur = cur->right;
+void outerleft(Node* root,vector<int> &res){
+    Node* curr = root->left;
+    
+    while(curr){
+        if(!isLeaf(curr)){
+            res.push_back(curr->data);
+        }
+        if(curr->left){
+            curr = curr->left;
+        }
+        else{
+            curr = curr->right;
+        }
     }
 }
 
-// Add right boundary nodes in reverse order (excluding leaves)
-void addRightBoundary(Node* root, vector<int>& res) {
-    Node* cur = root->right;
-    vector<int> tmp;
-    while (cur) {
-        if (!isLeaf(cur))
-            tmp.push_back(cur->data);
-        if (cur->right)
-            cur = cur->right;
-        else
-            cur = cur->left;
+void outerrightleaf(Node* root,vector<int> &res){
+    Node* curr = root->right;
+    vector<int> temp;
+    while(curr){
+        if(!isLeaf(curr)){
+            temp.push_back(curr->data);
+        }
+        
+        if(curr->right){
+            curr = curr->right;
+        }
+        else{
+            curr = curr->left;
+        }
     }
-    // Push in reverse to get bottom-to-top order
-    for (int i = tmp.size() - 1; i >= 0; --i)
-        res.push_back(tmp[i]);
+    
+    reverse(temp.begin(),temp.end());
+    
+    for(int x : temp){
+        res.push_back(x);
+    }
 }
 
-// Add all leaf nodes (left to right)
-void addLeaves(Node* root, vector<int>& res) {
-    if (isLeaf(root)) {
-        res.push_back(root->data);
+void addingallleafNodes(Node* root,vector<int> &res){
+    Node* curr = root;
+    
+    if(root==nullptr){
         return;
     }
-    if (root->left)
-        addLeaves(root->left, res);
-    if (root->right)
-        addLeaves(root->right, res);
-}
+    
+    if(isLeaf(curr)){
+        res.push_back(curr->data);
+        return;
+    }
 
-// Main function: Print boundary of binary tree
-vector<int> printBoundary(Node* root) {
-    vector<int> res;
-    if (!root) return res;
-
-    // Add root if it's not a leaf
-    if (!isLeaf(root))
-        res.push_back(root->data);
-
-    // Add left boundary (top to bottom, excluding leaves)
-    addLeftBoundary(root, res);
-
-    // Add all leaves (left to right)
-    addLeaves(root, res);
-
-    // Add right boundary (bottom to top, excluding leaves)
-    addRightBoundary(root, res);
-
-    return res;
+        addingallleafNodes(curr->left,res);
+        addingallleafNodes(curr->right,res);
 }
